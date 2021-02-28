0e76f88c3199ecd2f4b4ef4956a41bf308c2f11a
Ubuntu <ubuntu@ip-172-31-49-17.ec2.internal>
2021-02-28 04:58:45 +0000

    Polls App added.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Created Apps
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
